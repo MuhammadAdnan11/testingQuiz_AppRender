03bfbf4c66e12dd9e568b0937bce98cf6d1ddc15
MuhammadAdnan11 <adnan191022@gmail.com>
2025-05-26 15:32:06 +0500

    added

--- server.js
+++ server.js
@@ -117,7 +117,6 @@ const galleryRoutes = require('./routes/gallery'); // <-- make sure this file ex
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const path = require('path');
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 // ✅ Import admin routes and adminAuth middleware
